899b71187c4ed0b03b0234ae5e673812aa8ee4e1
Jam <electrojam@hotmail.com>
2024-05-11 16:28:37 +0000

    PCA y IPCA con 3 features, accuracy

--- pca.py
+++ pca.py
@@ -22,3 +22,29 @@ if __name__ == "__main__":
 
     X_train, X_test, y_train, y_test = train_test_split(dt_features, dt_target, test_size=0.30, random_state=42) #partimos conjunto de datos
     
+    print(X_train.shape) # verificamos éxito datos partidos con forma de var
+    print(y_train.shape)
+
+    pca = PCA(n_components=3) #configuramos algorit PCA
+    pca.fit(X_train)    #entrenamos
+
+    ipca = IncrementalPCA(n_components=3, batch_size=10) #configuramos algorit IPCA, manda entrena datos por lotes
+    ipca.fit(X_train)   #entrenamos
+    
+    plt.plot(range(len(pca.explained_variance_)), pca.explained_variance_ratio_)
+    plt.show()
+    
+    logistic = LogisticRegression(solver='lbfgs') #definimos var con función regresión logística
+
+    dt_train = pca.transform(X_train) #aplicamos PCA a conjuntos de entrenamiento y de prueba
+    dt_test = pca.transform(X_test)
+    logistic.fit(dt_train, y_train) #aplicamos regresión logística 
+
+    print("SCORE PCA:", logistic.score(dt_test, y_test)) #medimos efectividad del modelo, accuracy
+
+    dt_train = ipca.transform(X_train) #aplicamos IPCA a datos de prueba y entrenamiento
+    dt_test = ipca.transform(X_test)
+    logistic.fit(dt_train, y_train) #aplicamos regresión logística 
+
+    print("SCORE IPCA:", logistic.score(dt_test, y_test))
+    
